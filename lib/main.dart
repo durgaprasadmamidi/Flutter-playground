7fe1201d10ac8be4e39f5c7ab39659fb69678c23
durga prasad <durgaprasad.mamidi1608@gmail.com>
2020-06-19 11:58:37 +0530

    image added

--- lib/main.dart
+++ lib/main.dart
@@ -12,28 +12,52 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home:  Mainscreen(),
-          );
-        }
-      }
-      
-      class Mainscreen extends StatefulWidget {
-        Mainscreen({Key key}) : super(key: key);
-      
-        @override
-        _MainscreenState createState() => _MainscreenState();
-      }
-      
-      class _MainscreenState extends State<Mainscreen> {
-        @override
-        Widget build(BuildContext context) {
-          return Scaffold(
-            appBar: AppBar(title:Text("Flutter Playground"),),
-            floatingActionButton: FloatingActionButton(
-              child:Icon(Icons.add),
-              onPressed: () {
-                
-              },),
-          );
-        }
-      }
+      home: Mainscreen(),
+    );
+  }
+}
+
+class Mainscreen extends StatefulWidget {
+  Mainscreen({Key key}) : super(key: key);
+
+  @override
+  _MainscreenState createState() => _MainscreenState();
+}
+
+class _MainscreenState extends State<Mainscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Flutter Playground"),
+      ),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: Column(
+            children: [
+              Center(
+                  child: Image.network(
+                "https://images.unsplash.com/photo-1592430571922-763fca445247?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+                fit: BoxFit.cover,
+              )),
+              SizedBox(
+                height: 15,
+              ),
+              Text("Durga Prasad Mamidi"),
+              SizedBox(
+                height: 15,
+              )
+            ],
+          ),
+        ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
