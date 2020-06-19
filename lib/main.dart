bf4e1964622baa52c2415e41186d4a901d98279c
durga prasad <durgaprasad.mamidi1608@gmail.com>
2020-06-19 12:02:06 +0530

    added image

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,28 @@ class MyApp extends StatelessWidget {
         Widget build(BuildContext context) {
           return Scaffold(
             appBar: AppBar(title:Text("Flutter Playground"),),
+           body: SingleChildScrollView(
+          child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: Column(
+            children: [
+              Center(
+                  child: Image.network(
+                "https://images.unsplash.com/photo-1592430571922-763fca445247?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+                fit: BoxFit.cover,
+              )),
+              SizedBox(
+                height: 15,
+              ),
+              Text("Durga Prasad Mamidi"),
+              SizedBox(
+                height: 15,
+              )
+            ],
+          ),
+        ),
+      )),
             floatingActionButton: FloatingActionButton(
               child:Icon(Icons.add),
               onPressed: () {
